ffb8a7ee281f59c62402e47d92a60db449e10ace
mat <github@matdoes.dev>
2022-07-15 14:01:58 -0500

    wait longer before starting

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ function start() {
   bot.on('spawn', () => {
     if (!spawned)
       // wait for chunks to load and stuff
-      setTimeout(() => startFarming(bot), 2000)
+      setTimeout(() => startFarming(bot), 4000)
 
     spawned = true
     console.log('spawned')
