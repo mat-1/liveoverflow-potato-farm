9a012260bd168bc578696b2296dd4fbd2ce911a9
mat <github@matdoes.dev>
2022-07-15 13:56:45 -0500

    Update constants.ts

--- src/farm/constants.ts
+++ src/farm/constants.ts
@@ -2,8 +2,9 @@ import { Vec3 } from 'vec3'
 
 
 export const START_POS = new Vec3(-16, 64, 79)
-export const END_POS = new Vec3(-105, 64, 162)
+export const END_POS = new Vec3(-105, 64, 168)
 export const STORAGE_AREA = new Vec3(-21, 64, 76)
+export const DEPOSIT_CHEST = new Vec3(-32, 64, 77)
 
 // export const START_POS = new Vec3(16, 64, 8)
 // export const END_POS = new Vec3(-16, 64, 43)
