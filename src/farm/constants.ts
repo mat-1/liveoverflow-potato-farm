63478d40e1f236b0d0c7dfd3e2ce98f85260a03a
mat <github@matdoes.dev>
2022-07-15 01:45:07 -0500

    coords

--- src/farm/constants.ts
+++ src/farm/constants.ts
@@ -1,12 +1,13 @@
 import { Vec3 } from 'vec3'
 
 
-// export const START_POS = new Vec3(-16, 64, 79)
-// export const END_POS = new Vec3(-105, 64, 162)
+export const START_POS = new Vec3(-16, 64, 79)
+export const END_POS = new Vec3(-105, 64, 162)
+export const STORAGE_AREA = new Vec3(-21, 64, 76)
 
-export const START_POS = new Vec3(16, 64, 8)
-export const END_POS = new Vec3(-16, 64, 43)
-export const STORAGE_AREA = new Vec3(15, 64, 3)
+// export const START_POS = new Vec3(16, 64, 8)
+// export const END_POS = new Vec3(-16, 64, 43)
+// export const STORAGE_AREA = new Vec3(15, 64, 3)
 
 export const FARM_WIDTH = Math.abs(END_POS.x - START_POS.x)
 export const FARM_LENGTH = Math.abs(END_POS.z - START_POS.z)
